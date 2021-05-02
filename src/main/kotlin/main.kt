2c71b57121019dbeb4cc35542d0b2729ee1243d5
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-02 10:06:32 -0700

    add logic for user turn in Pig Dice

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,47 @@
 fun main() {
     //play Pig Dice
     var compScore = 0
-    //var userScore = 0
-    while (compScore < 100) {
+    var userScore = 0
+    while (compScore < 100 && userScore < 100) {
         compScore += compTurn()
         println("Computer total score: $compScore")
+        userScore += userTurn()
+        println("User total score: $userScore")
     }
-    println("Final Score: $compScore")
+    println("Final Computer Score: $compScore")
+    println("Final User Score: $userScore")
+    println(declareWinner(userScore, compScore))
 }
 
-//fun userTurn(){
-    //user turn
-//}
+fun userTurn(): Int{
+    var x = 0
+    var turnScore = 0
+    var roll=0
+    var choice = "y"
+    while (choice == "y") {
+        roll = rollDice()
+        println("User: Rolled $roll")
+        if (roll == 1) {
+            return 0
+        } else {
+            turnScore += roll
+        }
+        println("Your turn score is $turnScore. Would you like to roll again?")
+        choice = readLine().toString()
+        while(choice!="y" && choice!="n"){
+            println("Please enter y for yes, n for no")
+            choice = readLine().toString()
+        }
+    }
+    return turnScore
+}
 
 fun compTurn(): Int {
     var x = 0
     var turnScore = 0
     var roll=0
-    while (x < 3) {
-        roll = (Math.random() * 6).toInt() + 1
+    while (turnScore < 10) {
+        roll = rollDice()
         println("Computer: Rolled $roll")
         if (roll == 1) {
             return 0
@@ -28,4 +51,10 @@ fun compTurn(): Int {
         x ++
     }
     return turnScore
+}
+
+fun rollDice(): Int = (Math.random() * 6).toInt() + 1
+
+fun declareWinner(userScore: Int, compScore: Int): String {
+    return if(userScore > compScore) "User Wins!" else "Computer Wins!"
 }
